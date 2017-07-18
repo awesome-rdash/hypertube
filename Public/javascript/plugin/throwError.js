9ab10f1db062000bf7920fcd255628dd71f3e48c
Jeremy MISITI <jmisiti@e1r12p11.42.fr>
2017-07-18 15:43:12 +0200

    Mise a la norme

--- Public/javascript/plugin/throwError.js
+++ Public/javascript/plugin/throwError.js
@@ -1,22 +1,17 @@
-function throwError(type, mode)
-{
-  if (mode == 1)
-  {
-    $(".form-group").addClass("has-warning");
-    $("#i1 small").html($(type).html());
-  }
-  else if (mode == 2) {
-    $(".form-group").addClass("has-warning");
-    if (!$("#i1 small").val())
-      $("#i1 small").html($(type).html());
-    if (!$("#i2 small").val())
-      $("#i2 small").html($(type).html());
-  }
-  $("#b1, #b3, #b2").prop('disabled', false);
+function throwError(type, mode) {
+	if (mode === 1) {
+		$('.form-group').addClass('has-warning');
+		$('#i1 small').html($(`#${type}`).html());
+	} else if (mode === 2) {
+		$('.form-group').addClass('has-warning');
+		$('#i1 small').html($(`#${type}`).html());
+		$('#i2 small').html($(`#${type}`).html());
+	}
+	$('#b1, #b3, #b2').prop('disabled', false);
 }
 function stopError() {
-  $(".form-group").removeClass("has-warning");
-  $(".form-group").removeClass("has-success");
-  $("#i1 small").html("");
-  $("#i2 small").html("");
+	$('.form-group').removeClass('has-warning');
+	$('.form-group').removeClass('has-success');
+	$('#i1 small').html('');
+	$('#i2 small').html('');
 }
