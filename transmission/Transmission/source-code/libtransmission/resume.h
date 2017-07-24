635e9a223168640481dc77478ce2d321e07b0110
Justin <jrouzier@outlook.com>
2017-07-24 18:35:26 +0200

    Resume

--- transmission/Transmission/source-code/libtransmission/resume.h
+++ transmission/Transmission/source-code/libtransmission/resume.h
@@ -36,7 +36,8 @@ enum
   TR_FR_TIME_SEEDING        = (1 << 18),
   TR_FR_TIME_DOWNLOADING    = (1 << 19),
   TR_FR_FILENAMES           = (1 << 20),
-  TR_FR_NAME                = (1 << 21)
+  TR_FR_NAME                = (1 << 21),
+  TR_FR_SEQUENTIAL          = (1 << 22)
 };
 
 /**
