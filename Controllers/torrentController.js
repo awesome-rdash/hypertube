0d2c7d83ffd84f29cff41d48836929dac1860dfa
Justin ROUZIER <jrouzier@outlook.com>
2017-07-27 15:07:06 +0200

    Norme

--- Controllers/torrentController.js
+++ Controllers/torrentController.js
@@ -1,23 +1,25 @@
-var Transmission = require('transmission');
+Transmission = require('transmission');
 
-var transmission = new Transmission({
-	port: "9091",			// DEFAULT : 9091
-	host: "127.0.0.1",			// DEFAULT : 127.0.0.1
+transmission = new Transmission({
+	port: '9091',			// DEFAULT : 9091
+	host: '127.0.0.1',			// DEFAULT : 127.0.0.1
 	username: '',	// DEFAULT : BLANK
-	password: ''	// DEFAULT : BLANK
+	password: '',	// DEFAULT : BLANK
 });
 
-function getTransmissionStats(){
-	transmission.sessionStats(function(err, result){
-		if(err){
-			console.log(err);
-		} else {
-			console.log(result);
-		}
-	});
+function callback(err, result) {
+	if (err) {
+		console.log(err);
+	} else {
+		console.log(result);
+	}
+}
+
+function getTransmissionStats() {
+	transmission.sessionStats(callback(err, result));
 }
 
 exports.startTorrentDl = (req, res) => {
-	transmission.addUrl(req.body.magnet, function(err, arg){});
+	transmission.addUrl(req.body.magnet, callback(err, result));
 	getTransmissionStats();
 };
