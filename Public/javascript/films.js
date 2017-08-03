dc7478916e24483208e77748d85cb82c82aedf1f
Jeremy MISITI <jmisiti@e1r8p5.42.fr>
2017-08-03 15:25:33 +0200

    Ajout de la zone comment

--- Public/javascript/films.js
+++ Public/javascript/films.js
@@ -1,6 +1,7 @@
 $(document).ready(() => {
 	$('#comment').keypress((e) => {
 		if (e.which === 13) {
+			console.log($('#comment').val() + ' sended');
 			$.post('/comment', { movieId: '0', com: $('#comment').val() }, (data) => {
 				if (data === true) {
 					$('#commentZone').append('Salut');
