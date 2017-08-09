edbae99c5cf0b4d5c196d11dd73c129ecf4ed02b
behamon <behamon@student.42.fr>
2017-08-09 18:13:35 +0200

    send user

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -143,7 +143,7 @@ exports.getUsersByUsername = async (req, res) => {
 			ret.push(user);
 			console.log(user);
 			if (!fs.existsSync(path.join(__dirname, `../Public/${user.photo}`))) {
-				ret[i].photo = null;
+				ret[i].photo = '/assets/empty_user.png';
 			}
 		});
 		return res.json(ret);
@@ -156,6 +156,9 @@ exports.userPage = async (req, res) => {
 	proms.push(User.findOne({ _id: req.params.id }, { username: 1, photo: 1 }));
 	proms.push(Comment.find({ author: req.params.id }).sort({ posted: -1 }).limit(10));
 	const [user, coms] = await Promise.all(proms);
+	if (!fs.existsSync(path.join(__dirname, `../Public/${user.photo}`))) {
+		user.photo = '/assets/empty_user.png';
+	}
 	const ret = {
 		id: user._id,
 		username: user.username,
