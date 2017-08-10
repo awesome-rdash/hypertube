8d8a0bdb5c5831a5084c7d962f5d913a6e6501bd
Justin ROUZIER <jrouzier@outlook.com>
2017-08-10 14:37:24 +0200

    Adding remote to builds

--- run_daemon.sh
+++ run_daemon.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
 xcodebuild -project "transmission/source-code/Transmission.xcodeproj" -target transmission-daemon -configuration Release build
+xcodebuild -project "transmission/source-code/Transmission.xcodeproj" -target transmission-remote -configuration Release build
+
 cp -v -a "transmission/source-code/build/Release/." "transmission/build/"
 
 cp -v -a "transmission/source-code/web/." "transmission/build/web/"
-export TRANSMISSION_WEB_HOME=transmission/source-code/web/
+
+export TRANSMISSION_WEB_HOME="$(pwd)/transmission/build/web/"
+echo "Web folder is set to: $TRANSMISSION_WEB_HOME";
 
 transmission/build/transmission-daemon -f
